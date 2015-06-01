da8f2df66afbdfc2ea565451cf8fb4e9d9d378f0
Eugene Keeper <keeper.biker@gmail.com>
2015-06-01 22:33:19 +0300

    oudatedBrowser, bootstrap, style.less, media.less

---
+++ js/scripts.js
@@ -0,0 +1,46 @@
+/*  Equal height blocks :: http://css-tricks.com/equal-height-blocks-in-rows/ */
+
+equalheight = function(container){
+    var currentTallest = 0,
+        currentRowStart = 0,
+        rowDivs = new Array(),
+        $el,
+        topPosition = 0;
+    $(container).each(function() {
+        $el = $(this);
+        $($el).height('auto')
+        topPostion = $el.position().top;
+
+        if (currentRowStart != topPostion) {
+            for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
+                rowDivs[currentDiv].height(currentTallest);
+            }
+            rowDivs.length = 0; // empty the array
+            currentRowStart = topPostion;
+            currentTallest = $el.height();
+            rowDivs.push($el);
+        } else {
+            rowDivs.push($el);
+            currentTallest = (currentTallest < $el.height()) ? ($el.height()) : (currentTallest);
+        }
+        for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
+            rowDivs[currentDiv].height(currentTallest);
+        }
+    });
+}
+
+$(window).load(function() {
+    equalheight('.ba-grid .du-item .du-desc');
+});
+
+$(window).resize(function(){
+    equalheight('.ba-grid .du-item .du-desc');
+});
+
+/* Menu toggle function */
+jQuery(function($){
+    $('.du-menu-toggle, .du-menu-overlay').click(function(){
+        $('body').toggleClass('du-menu-open');
+    });
+});
+
